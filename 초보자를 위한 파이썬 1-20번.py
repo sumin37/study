649a37ad19d5943ba4dc9679c8caa6f684d4f1f9
sumin37 <smp9554@gmail.com>
2023-02-18 22:35:35 +0900

    Create 초보자를 위한 파이썬 1-20번.py

---
+++ 초보자를 위한 파이썬 1-20번.py
@@ -0,0 +1,78 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+print("Hello World")
+
+print("Mary's cosmetics")
+
+print('신씨가 소리질렀다. "도둑이야"')
+
+print('"C:\Windows"')
+
+print("안녕하세요.\n만나서 \t\t반갑습니다.")
+#t : tab, \n : 줄바꿈
+
+print("오늘은","일요일")
+
+print("naver;kakao;samsung")
+
+print("naver/kakao/samsung")
+
+print("frist");print("second")
+
+5/3
+
+삼성전자 = 500000
+총평가금액 = 삼성전자*10
+
+총평가금액
+
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+
+s = "hello"
+t = "python"
+print(s + "! " + t)
+
+2 + 2*3
+
+a = 128
+print(type(a))
+
+a ="132"
+print(type(a))
+
+num_str ="720"
+int(num_str)
+
+num = 100
+str(num)
+
+a = "15.79"
+float(a)
+
+year = "2020"
+print(int(year)-3)
+print(int(year)-2)
+print(int(year)-1)
+
+
+월 =48584
+총금액 = 월*36
+print(총금액)
